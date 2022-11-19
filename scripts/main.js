9787b6fbfffda0e3b92ee9a2489bf9c66cc4fe40
Nikolay Belokonenko <belokonenkons@gmail.com>
2022-11-19 11:55:48 +0200

    site page first upload

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+var myHeading = document.querySelector('.logo-title');
+myHeading.textContent = 'Hello from JS, CandyBar!';
+
+document.querySelector('.page').onclick = function() {
+    alert('Ouch! Stop poking me!');
+}
